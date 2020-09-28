9db1bcd2f43979b01c1d60c0367b8e3ccb51322a
Ariel Leon <arielleon@MacBook-Pro.local>
2020-09-28 01:11:05 -0400

    pair programing word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,6 +3,36 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
+const transpose = function(matrix) {
+  let temp = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    temp.push([]);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0 ; j < matrix[i].length; j++) {
+        temp[j][i] = matrix[i][j];
+    }
+  }
+  return temp;
+};
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(‘’))
+  for (let l of horizontalJoin) {
+      if(l.includes(word)) {
+          return true
+      }
+  }
+  const newLetters = transpose(letters)
+  const horizontalJoin2 = newLetters.map(ls => ls.join(‘’))
+  for (let l of horizontalJoin2) {
+      if(l.includes(word)) {
+          return true
+          }
+      }
+      return false
+  }
+module.exports = wordSearch
+
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
